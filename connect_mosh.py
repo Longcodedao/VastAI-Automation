50e41e339d1251b5c67484d4c2a3a2006fceddbb
Longcodedao <long.vo210404@vnuk.edu.vn>
2025-11-15 11:02:22 +0700

    Finish connect to mosh terminal code

--- connect_mosh.py
+++ connect_mosh.py
@@ -4,7 +4,7 @@ import json
 import sys
 import subprocess
 import re
-from utils import run_vast_command
+from utils import run_vast_command, run_command
 
 
 def parse_args():
@@ -23,7 +23,7 @@ def parse_args():
         default=60001,
         help="UDP Port for connecting the Mosh shell",
     )
-    return parser
+    return parser.parse_args()
 
 
 def main():
@@ -40,20 +40,62 @@ def main():
         out_json = json.loads(show_output)
 
         if not out_json:
-            print(f"There is no instance. Shut down")
+            print("There is no instance. Shut down")
             sys.exit(1)
 
         public_ip = out_json.get("public_ipaddr", None)
         ports = out_json.get("ports")
 
-        mosh_ext_port= ports.get(f"{args.port_udp}/udp")
-        ssh_ext_port = ports.get("22/tcp") 
+        mosh_ext_port = ports.get(f"{args.port_udp}/udp")[0].get("HostPort")
+        ssh_ext_port = ports.get("22/tcp")[0].get("HostPort")
 
         if not public_ip or not ports or not ssh_ext_port:
             print("Error: Missing required components for connection (IP, Ports, ...)")
-           sys.exit(1) 
-       
-         
+            sys.exit(1)
 
     except Exception as e:
         print(f"Unexpected Error has occured: {e}")
+
+    mosh_cmd = f"mosh-server new -p {args.port_udp}"
+    ssh_cmd = [
+        "ssh",
+        "-p",
+        ssh_ext_port,
+        f"root@{public_ip}",
+        "-L",
+        "8080:localhost:8080",
+        mosh_cmd,
+    ]
+
+    print("Connecting to the SSH to get the Mosh key ...")
+
+    try:
+        ssh_result = run_command(ssh_cmd)
+        # mosh_key = re.search(r"^[A-Za-z0-9+/]+={0,2}$", ssh_result)
+        mosh_key = ssh_result.split(" ")[-1]
+        print(ssh_result)
+        print(f"Mosh key is: {mosh_key}")
+        if not mosh_key:
+            print("Error Cannot find a valid Mosh Key. Exiting ....")
+            sys.exit(1)
+
+    except Exception as e:
+        print(f"Unexpected error when connecting SSH: {e}")
+        sys.exit(1)
+
+    print("Launching the MOSH shell")
+
+    os.environ["MOSH_KEY"] = mosh_key
+
+    mosh_client_cmd = [
+        "mosh-client",
+        public_ip,
+        # Tell mosh which external UDP port to use
+        str(mosh_ext_port),
+    ]
+
+    subprocess.run(mosh_client_cmd)
+
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -34,6 +34,36 @@ def run_vast_command(
         raise
 
 
+def run_command(command_list: list, check: bool = True, capture_output: bool = True):
+    """
+    Executes a Bash CLI command.
+
+    :param command_args: List of arguments to pass to 'vastai'.
+    :param check: If True, raise an exception on non-zero exit status.
+    :param capture_output: If True, capture stdout/stderr and return it.
+    :return: The output (stdout) as a string, or the subprocess result object.
+    """
+    try:
+        result = subprocess.run(
+            command_list, check=check, capture_output=capture_output, text=True
+        )
+        # print(f"Result is: {result}")
+        # print(f"Stdout of result: {result}")
+        if capture_output:
+            return result.stdout.strip()
+
+        return result
+
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed and return code: {e.returncode}")
+        print(f"STDERR: {e.stderr}")
+        raise
+
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        raise
+
+
 def cleanup_instance(instance_id: str):
     """Destroys the instance if the ID is valid."""
     # CLI instance IDs are typically strings
